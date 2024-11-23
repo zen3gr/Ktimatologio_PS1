541bde61315d47b3ec254101c0c3a3804289eb28
zen3gr <88621786+zen3gr@users.noreply.github.com>
2024-11-23 22:11:27 +0200

    kt_posthandler moved to posthandler

--- ktima_req.ps1
+++ ktima_req.ps1
@@ -10,27 +10,27 @@ $body = @{
 }
 
 $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
-$session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-$session.Cookies.Add((New-Object System.Net.Cookie(".AspNetCore.Antiforgery.OvFV5KRnAAc", "CfDJ8OSBp992T1dLoV-idSwOs9fMjJZaRLVdgp4pYCYl5vD4TIyS6QGvFBLVm_srkwgVC5pR7iGH-bvBL40zFErkmW1kX521gWgfqsMY6iMooOZcR9xIF2RI1BMb3y_0z1BOM2O0VM1vatv5rtGTcNuAubs", "/", "maps.gov.gr")))
-$session.Cookies.Add((New-Object System.Net.Cookie("cookiesession1", "678A3E46A0B24D9D26D504DC967435F8", "/", "maps.gov.gr")))
+$session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+$session.Cookies.Add((New-Object System.Net.Cookie(".AspNetCore.Antiforgery.OvFV5KRnAAc", "CfDJ8PRpNdCwqcVNmpkXHWjcfvfp2qvZi2sVPuXczAWjNJqkshshaRF9Zau53pSkSLFi1nMuDn5nyL3vsJuBd9tnO2vL4U6vnIHwuJMfK0tJrHTX22HeR4zkWvl6Yos-tRBPbvTBuHUPybXH6OQtMs0KC70", "/", "maps.gov.gr")))
+$session.Cookies.Add((New-Object System.Net.Cookie("cookiesession1", "678A3E460F7C0D32C69829E7CBB151B8", "/", "maps.gov.gr")))
 try
 {
-$response = Invoke-WebRequest -Uri "https://maps.gov.gr/gis/map/KT/KT__PostHandlerCommon" `
+$response = Invoke-WebRequest -Uri "https://maps.gov.gr/gis/map/PostHandler" `
 -Method POST `
 -WebSession $session `
 -Headers @{
 "Accept"="*/*"
   "Accept-Encoding"="gzip, deflate, br, zstd"
-  "Accept-Language"="en-GB,en;q=0.9"
+  "Accept-Language"="en-GB,en-US;q=0.9,en;q=0.8,el;q=0.7"
   "DNT"="1"
   "Origin"="https://maps.gov.gr"
   "Referer"="https://maps.gov.gr/gis/map/"
-  "RequestVerificationToken"="CfDJ8OSBp992T1dLoV-idSwOs9foel5n6jHR8lF7oooa3uwMLeOmWc_EG12t7b8UVrR1AINO3YN4h2_I2d-hIFY_Wq1R-xyksWj-D3uick8dbzSSsLFBA6Cn9-KOrJJSqcS6Ph_G7LgQUalmr6XPKS_LLZM"
+  "RequestVerificationToken"="CfDJ8PcttteAo_dFgirjhuO2cKk1cjNsypa5aVuX-OjibA8OjAn4-z6IHdQ2MtD0kHwoBjMgJh8YJ2mPLm7Gnp2CDC6fdmtL_UTq9N-mAA69lq0bx0EHcBGEKxDDSl1eOkbl5XNt1ONmRw0ckugjNzgaCV0"
   "Sec-Fetch-Dest"="empty"
   "Sec-Fetch-Mode"="cors"
   "Sec-Fetch-Site"="same-origin"
   "X-Requested-With"="XMLHttpRequest"
-  "sec-ch-ua"="`"Chromium`";v=`"122`", `"Not(A:Brand`";v=`"24`", `"Google Chrome`";v=`"122`""
+  "sec-ch-ua"="`"Google Chrome`";v=`"131`", `"Chromium`";v=`"131`", `"Not_A Brand`";v=`"24`""
   "sec-ch-ua-mobile"="?0"
 
 
